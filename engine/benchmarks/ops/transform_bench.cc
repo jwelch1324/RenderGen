1c20d8c4a2a303805b405e87045b0da5b06b0117
Jonathan <jwelch@sensoryinc.com>
2022-04-26 20:55:32 -0400

    Added benchmarking coding

---
+++ engine/benchmarks/ops/transform_bench.cc
@@ -0,0 +1,34 @@
+#include "geometry/bbox.h"
+#include "geometry/point.h"
+#include "ops/transform.h"
+#include <benchmark/benchmark.h>
+
+using namespace rengen::geometry;
+using namespace rengen::ops;
+
+static void BM_BBoxDefaultTransform(benchmark::State &state) {
+  BBox b0(Point3f(0, 0, 0), Point3f(1, 1, 1));
+  auto tr = Translate(Vec3f(1, 1, 1));
+  for (auto _ : state)
+    auto newBB = tr(b0);
+}
+
+static void BM_BBoxSlowTransform(benchmark::State &state) {
+  BBox b0(Point3f(0, 0, 0), Point3f(1, 1, 1));
+  auto tr = Translate(Vec3f(1, 1, 1));
+  for (auto _ : state)
+    auto newBB = tr.SlowTransform(b0);
+}
+
+static void BM_BBoxFastTransform(benchmark::State &state) {
+  BBox b0(Point3f(0, 0, 0), Point3f(1, 1, 1));
+  auto tr = Translate(Vec3f(1, 1, 1));
+  for (auto _ : state)
+    auto newBB = tr.FastTransform(b0);
+}
+
+BENCHMARK(BM_BBoxDefaultTransform);
+BENCHMARK(BM_BBoxSlowTransform);
+BENCHMARK(BM_BBoxFastTransform);
+
+BENCHMARK_MAIN();
---
+++ engine/tests/ops/transform_test.cc
@@ -0,0 +1,23 @@
+#include "geometry/bbox.h"
+#include "geometry/point.h"
+#include "ops/transform.h"
+#include <gtest/gtest.h>
+
+using namespace rengen::ops;
+using namespace rengen::geometry;
+
+TEST(BBoxTransformTest, BasicAssertions) {
+  BBox b0(Point3f(0, 0, 0), Point3f(1, 1, 1));
+
+  auto tr = Translate(Vec3f(1, 1, 1));
+
+  auto newBB = tr(b0);
+  auto newFastBB = tr.FastTransform(b0);
+  EXPECT_TRUE(newBB.epsilonEqual(newFastBB));
+
+  auto str = Scale(Vec3f(1, 2, 3));
+
+  newBB = str(b0);
+  newFastBB = str(b0);
+  EXPECT_TRUE(newBB.epsilonEqual(newFastBB));
+}
