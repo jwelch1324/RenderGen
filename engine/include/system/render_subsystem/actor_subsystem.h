58d786a147257036c233823b081ae7a9c2cc63fc
Jonathan <jwelch@sensoryinc.com>
2022-06-27 15:26:17 -0400

    Starting work on actor based render subsystem

---
+++ engine/include/system/render_subsystem/actor_subsystem.h
@@ -0,0 +1,10 @@
+#ifndef __ACTOR_SUBSYSTEM_H__
+#define __ACTOR_SUBSYSTEM_H__
+// This subsystem uses CAF to create distributed actors that will perform a
+// rendering
+
+#define ENABLE_CAF
+#ifdef ENABLE_CAF
+
+#endif
+#endif // __ACTOR_SUBSYSTEM_H__
